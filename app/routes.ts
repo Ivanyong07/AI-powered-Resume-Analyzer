2a6aa5ce85b3169d1049f34ffb39aaed63ab1420
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-06-01 14:57:14 +0800

    Resume Feedback

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ import path from "path";
 export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/Auth.tsx'),
-    route('/upload', 'routes/Upload.tsx')
+    route('/upload', 'routes/Upload.tsx'),
+    route('/resume/:id', 'routes/Resume.tsx')
 ] satisfies RouteConfig;
