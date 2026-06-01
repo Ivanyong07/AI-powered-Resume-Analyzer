3c3961fdf5b25e31a66804ae17b3ddf2ace3ea3a
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-06-01 16:27:01 +0800

    Wipe data

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,6 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/Auth.tsx'),
     route('/upload', 'routes/Upload.tsx'),
-    route('/resume/:id', 'routes/Resume.tsx')
+    route('/resume/:id', 'routes/Resume.tsx'),
+    route('/wipe', 'routes/wipe.tsx')
 ] satisfies RouteConfig;
